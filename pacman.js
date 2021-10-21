9286f7d7b3b1eb39e165dac68d6c160a47779c52
QuentinChevaux <quentin.chevaux@gmail.com>
2021-10-21 16:08:59 +0200

    PacMan

--- pacman.js
+++ pacman.js
@@ -1,3 +1,28 @@
+var favicon_images = [
+    '/images/favicon/frame_0_delay-0.1s.gif',
+    '/images/favicon/frame_1_delay-0.1s.gif',
+],
+image_counter = 0; // To keep track of the current image
+
+setInterval(function() {
+// remove current favicon
+if(document.querySelector("link[rel='icon']") !== null)
+document.querySelector("link[rel='icon']").remove();
+if(document.querySelector("link[rel='shortcut icon']") !== null)
+document.querySelector("link[rel='shortcut icon']").remove();
+
+// add new favicon image
+document.querySelector("head").insertAdjacentHTML('beforeend', '<link rel="icon" href="' + favicon_images[image_counter] + '" type="image/gif">');
+
+// If last image then goto first image
+// Else go to next image    
+if(image_counter == favicon_images.length -1)
+image_counter = 0;
+else
+image_counter++;
+}, 100);
+
+
 let grille = [                      
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 0],
@@ -28,28 +53,36 @@ let rows = 22;
 
 let niveau = document.getElementById("niveau_grid");
 
-for (let row = 0; row < rows; row++) {
+function play() {
 
-    for (let column = 0; column < columns; column++) {
+    niveau.innerHTML = ""
 
-        if (grille[row][column] == 0) {
-        let mur = document.createElement("div");
-        mur.className = "mur";
-        niveau.appendChild(mur);
+    for (let row = 0; row < rows; row++) {
+
+        for (let column = 0; column < columns; column++) {
+
+            if (grille[row][column] == 0) {
+            let mur = document.createElement("div");
+            mur.className = "mur";
+            niveau.appendChild(mur);
+            }
+
+            if (grille[row][column] == 1) {
+                let sol = document.createElement("div");
+                sol.className = "sol";
+                niveau.appendChild(sol);
+            }
+
+            if (grille[row][column] == 2) {
+                let bonbon = document.createElement("div");
+                bonbon.className = "bonbon";
+                niveau.appendChild(bonbon);
+            }
+
         }
-
-        if (grille[row][column] == 1) {
-            let sol = document.createElement("div");
-            sol.className = "sol";
-            niveau.appendChild(sol);
-        }
-
-        if (grille[row][column] == 2) {
-            let bonbon = document.createElement("div");
-            bonbon.className = "bonbon";
-            niveau.appendChild(bonbon);
-        }
-
     }
 }
+setInterval(play,500)
 
+
+    
